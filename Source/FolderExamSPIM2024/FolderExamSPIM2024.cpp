859e5061d50514e8e3696bb3c19faa7c684fc2cf
Per Kristian Coskun <47058816+Zartok89@users.noreply.github.com>
2024-11-21 19:12:19 +0100

    Initial commit

---
+++ Source/FolderExamSPIM2024/FolderExamSPIM2024.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FolderExamSPIM2024.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FolderExamSPIM2024, "FolderExamSPIM2024" );
